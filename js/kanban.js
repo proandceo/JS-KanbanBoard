f92b7bb1681bae1813304545ac03990d0410a649
proandceo <proandceo@gmail.com>
2023-02-04 06:31:29 +1000

    test1

--- js/kanban.js
+++ js/kanban.js
@@ -16,6 +16,9 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement("div");
+  item.classList.add(item);
+  
 };
 
 document.querySelectorAll('.drop').forEach(element => {
